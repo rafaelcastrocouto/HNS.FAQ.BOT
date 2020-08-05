fdd66569215c0debc8ceb788f9d004b49c7e1a58
@racascou <rafaelcastrocouto@gmail.com>
2020-08-05 01:17:07 +0000

    better history

--- main.py
+++ main.py
@@ -11,8 +11,8 @@ openai.api_key = os.environ.get('OPENAI')
 def score(e):
     return e.score
 
+def ask(log, answer=''):
 
-def ask(name, log, answer=''):
     responses = openai.Completion.create(
         engine = 'davinci',
         prompt = log, # prompt max tokens=2049
@@ -20,25 +20,30 @@ def ask(name, log, answer=''):
         temperature = 0.1, # Float value controlling randomness in boltzmann distribution.The model will become deterministic at zero
         stop = '\n')
 
-    print('openai', responses.choices[0].text)
+    print(log + responses.choices[0].text)
     answer += responses.choices[0].text
 
     if not len(answer):
-        answer = 'Sorry I could not understand. Please try at searching at our [Learning center](https://learn.namebase.io).'
-
-    # keep asking until it ends the sentence
-    if not answer.endswith('.'):
-        answer = ask(name, log, answer)
+        answer = "Please try at searching at our Learning center: https://learn.namebase.io."
 
     # remove bot name
-    if answer.startswith(name):
-        answer = answer[2 + len(name):]
+    if answer.startswith('A: '):
+        answer = answer[3:]
 
     return answer
 
 
 # read all datasets.txt
-file_names = ['auction', 'dns', 'domains', 'handshake', 'hns', 'wallets']
+file_names = [
+  'auction', 
+  'ca',
+  'dns', 
+  'domains', 
+  'handshake', 
+  'hns', 
+  'miner',
+  'wallets'
+]
 datasets = []
 for file_name in file_names:
     datafile = open('datasets/' + file_name + ".txt", "r")
@@ -60,6 +65,7 @@ def start_server():
 t = Thread(target=start_server)
 t.start()
 
+
 # create discord bot client
 CHANNEL_ID = 357574292541669377
 client = discord.Client()
@@ -76,21 +82,58 @@ async def on_ready():
 @client.event
 async def on_message(message):
     if message.channel.id == CHANNEL_ID and message.author != client.user:
-        # build prompt log
 
-        # semantic search for best faq
-        # get discord channel history
+        log = ''
+        # prompt log:
+        #
+        # best_history: search discord questions?
+        # A: Answers.
 
-        # best faq: questions?
+        # best_faq: search dataset questions?
         # BOT: Answers.
         #
-        # history: questions?
+        # last_history: discord last questions?
         # BOT: Answers.
         #
         # last: question?
         # BOT: ...
 
-        log = 'The following is a respectful conversation between a very polite attendant and a customer.\n\n'
+        # get channel history
+        channel_hist = []
+        async for msg in message.channel.history(limit=100):
+            channel_hist.append(msg)
+
+        n = 1
+        history = []
+        content = ''
+        for msg in reversed(channel_hist):
+            if msg.author.name == client.user.name:
+              content += 'A: ' + msg.content
+            else:
+              content += 'Q: ' + msg.content
+            for embed in msg.embeds:
+                content += '[' + str(embed.title) + '](' + str(embed.url) + ')'
+            content += '\n'
+            if msg.author.name == client.user.name:
+                history.append(content)
+                #print(content+ '\n');
+                content = ''
+
+        # search best in history
+        result = openai.Engine("davinci").search(
+            documents=history[:-n], query=message.content)
+
+        # create zip with scores and history
+        z = zip(map(score, result.data), history[:-n])
+
+        # create new array with history sorted by score
+        best_hist = [x for _,x in reversed(sorted(z))];
+
+        # best history matches
+        best_history = ''
+        for msg in best_hist[:n]:
+            best_history += msg
+        log += best_history
 
         # semantic search for best faq
         result = openai.Engine("davinci").search(
@@ -98,30 +141,33 @@ async def on_message(message):
         # sorts by score
         result.data.sort(reverse=True, key=score)
         best_faq = datasets[result.data[0].document]
-        log += best_faq
 
-        # get channel history
-        history = []
-        async for old_msg in message.channel.history(limit=5):
-            content = old_msg.author.name + ': ' + old_msg.content
-            for embed in old_msg.embeds:
-                content += '[' + str(embed.title) + '](' + str(embed.url) + ')'
-            if old_msg.author.name == client.user.name:
-                content += '\n'
-            history.append(content + '\n')
-        # reverse and convert to string
-        history_string = ''
-        for msg in reversed(history):
-            history_string += msg
-        log += history_string
-        msg = message.author.name + ': ' + message.content
+        # semantic search for best question
+        best_in_faq = best_faq.split('\n\n')
+        result = openai.Engine("davinci").search(
+            documents=best_in_faq, query=message.content)
+        # sorts by score
+        result.data.sort(reverse=True, key=score)
+        best_q = best_in_faq[result.data[0].document]
+
+        log += best_q
+
+        # last history
+        last_history = ''
+        for msg in history[-n:]:
+            last_history += msg
+        log += last_history
+
+        msg = 'Q: ' + message.content
         if not msg in log:
-            log += msg
+            log += msg + '\n'
 
-        print('discord', msg);
+        log += 'A: '
+
+        #print('## discord', msg + '\n');
 
         # ask openai and send response to discord
-        response = ask(client.user.name, log)
+        response = ask(log)
         await message.channel.send(response)
 
 
